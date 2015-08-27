ff0406202966d6d8ba7fa613bde707632fc5ebbd
shuster28 <nicolasshuster@gmail.com>
2015-08-26 21:00:01 -0700

    magic scroll by andrew
    magic scroll by andrew
    clean white footer with centred links and social

--- fp.js
+++ fp.js
@@ -1,16 +1,37 @@
 
-  (function(){
-    $(window).scroll(function() {
-    
-       /* // check if window scroll for more than 430px. May vary
-        // as per the height of your main banner.*/
-        
-        if($(this).scrollTop() > 430) { 
-            $('.navbar').addClass('opaque'); // adding the opaque class
-        } else {
-            $('.navbar').removeClass('opaque'); // removing the opaque class
-        }
-    });
-  })();
+$(document).ready(function(){
+  
+  /*
+  * Animate in the header bar on scroll
+  */  
+  $(window).scroll(function() {
+  
+     /* // check if window scroll for more than 430px. May vary
+      // as per the height of your main banner.*/
+      
+      if($(this).scrollTop() > 430) { 
+          $('.navbar').addClass('opaque'); // adding the opaque class
+      } else {
+          $('.navbar').removeClass('opaque'); // removing the opaque class
+      }
+  });
+
+  /*
+  * Scroll the body to a given section
+  */
+  $(document).on("click", ".navbar-nav a", function(event){
+    event.preventDefault();
+
+    var $this = $(this);
+    var targetLabel = $this.attr("href"); 
+    var destination = $("[data-target=" + targetLabel + "]").offset().top;
+
+    $("body, html").animate({
+      scrollTop: destination
+    }, 450, "swing");
+
+  });
+
+});
 
 
